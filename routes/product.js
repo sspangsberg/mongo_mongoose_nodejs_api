b2af3d95c9af8492bc21172a7575fe2f843a4bf5
sspangsberg <smsj@easv.dk>
2021-03-21 08:36:18 +0100

    Updated with independent workflow tests + include auth

--- routes/product.js
+++ routes/product.js
@@ -3,8 +3,8 @@ const product = require("../models/product");
 const { verifyToken } = require("../validation");
 
 // Create new product
-//router.post("/", verifyToken, (req, res) => {
-router.post("/", (req, res) => {
+router.post("/", verifyToken, (req, res) => {
+//router.post("/", (req, res) => {
     const data = req.body;
     product.insertMany(data)
         .then(data => { res.status(201).send(data); })
@@ -57,7 +57,7 @@ router.put("/:id", verifyToken, (req, res) => {
 router.delete("/:id", verifyToken, (req, res) => {
     const id = req.params.id;
 
-    product.findByIdAndRemove(id)
+    product.findByIdAndRemove(id,{useFindAndModify: false})
         .then(data => {
             if (!data) {
                 res.status(404).send({
--- test/ignore/product.test.js
+++ test/ignore/product.test.js
@@ -3,7 +3,8 @@ const chai = require('chai');
 const expect = chai.expect;
 const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const server = require('../../server');
+const Product = require('../../models/product');
 
 chai.use(chaiHttp);
 
