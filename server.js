bba061924f826b21e33f7737cf604a551a8ebfe1
sspangsberg <smsj@easv.dk>
2021-04-08 20:58:41 +0200

    Test update

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use("/api/products", productRoutes); //CRUD routes
 //start up server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, function () {
-  console.log("Server is running on port: " + PORT);
+  console.log("Server is running on port:  " + PORT);
 });
 
 module.exports = app;
