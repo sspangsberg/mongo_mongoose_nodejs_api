5d4babf47d22d7e6c19a117b10af5d4ada09fc60
sspangsberg <smsj@easv.dk>
2021-04-14 13:05:42 +0200

    Update with CORS

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Handle CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 //connect to the MongoDB using Mongoose ODM
 mongoose.connect (
   process.env.DBHOST,  { useUnifiedTopology: true, useNewUrlParser: true }
